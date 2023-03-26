f047dc93d22e1dad58f2794afd977ca3711880a4
VLykanZ <Suphawish707@gmail.com>
2023-03-26 12:50:23 +0700

    update picture in DoExercise

--- src/component/Exclassifier.jsx
+++ src/component/Exclassifier.jsx
@@ -2,6 +2,14 @@ import React, { useRef , useState, useEffect} from "react";
 import { useSelector } from 'react-redux';
 import {Classifier, MovenetComponent, model, prediction} from './MovenetComponent';
 import * as tf from "@tensorflow/tfjs";
+
+import neck1 from '../assets/images/neck1.jpg';
+import neck2 from '../assets/images/neck2.jpg';
+import back1 from '../assets/images/back1.jpg';
+import back2 from '../assets/images/back2.jpg';
+import body1 from '../assets/images/body1.jpg';
+import body2 from '../assets/images/body2.jpg';
+
 // Database
 const PoseList ={
   'back' : [0,1],
@@ -52,6 +60,29 @@ function Exclassifier(props) {
   const [ confident, setPoint] = useState("Start");
   const [imagePath, setImagePath] = useState(`../assets/images/model/${pose_list[pose_step]}.jpg`);
 
+  const exercises = {
+    neck: [
+      { name: 'Back Neck Stretch', duration: '15-30 Seconds' ,img: neck1},
+      { name: 'Side Neck Stretch', duration: '15-30 Seconds' ,img: neck2},
+    ],
+    back: [
+      { name: 'Subscapularis Stretch', duration: '15-30 Seconds' ,img: back1},
+      { name: 'Shoulder Adductor Stretch', duration: '15-30 Seconds' ,img: back2},
+    ],
+    body: [
+      { name: 'Forearm Stretch', duration: '15-30 Seconds' ,img: body1},
+      { name: 'Abdominal & Upper Body Strech', duration: '15-30 Seconds' ,img: body2},
+    ],
+  };
+
+  const muscles = {
+    neck: [{muscle: 'Neck & Shoulder'}],
+    back: [{muscle: 'Back'}],
+    body: [{muscle: 'Body & Arm'}]
+  }
+
+  const exerciseList = exercises[bodyPart];
+  const musclesList = muscles[bodyPart];
 
   const changeFrame = () => {
     if (processing){
@@ -67,8 +98,19 @@ function Exclassifier(props) {
 
   return (
     <div>
-      <img src= {imagePath} width={640} height={480}/>
-      <p>Pose name  {posename} {bodyPart} | Status {status}</p>
+      {/* <img src= {imagePath} width={640} height={480}/> */}
+      {/* <p><img src={neck1} alt="" style={{ height: "200px" }} /> </p> */}
+      {exerciseList.map((exercise, index) => (
+            <p className="exercise" key={index}>
+              <img src={exercise.img}  alt="" style={{ height: "200px" }}/> <br />
+              {exercise.name} : {exercise.duration}          
+            </p>         
+          ))}
+
+      {musclesList.map((muscle, index) => (
+        <p>Pose name  {posename} {muscle.muscle} | Status {status}</p>
+          ))}
+      
     </div>
   );
 }
