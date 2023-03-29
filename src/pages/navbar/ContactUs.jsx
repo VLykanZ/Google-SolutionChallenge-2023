73a78cdb7aa6d7b26b123f895c2ab884d52b6ada
VLykanZ <Suphawish707@gmail.com>
2023-03-30 04:17:19 +0700

    update contact us

--- src/pages/navbar/ContactUs.jsx
+++ src/pages/navbar/ContactUs.jsx
@@ -1,33 +1,71 @@
-
+import React, { useState } from "react";
 
 const ContactUs = () => {
+  const [formValues, setFormValues] = useState({
+    name: "",
+    email: "",
+    title: "",
+    body: "",
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Use the formValues state to send the email
+    console.log("Form submitted with values:", formValues);
+  };
 
   return (
     <body>
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <div>
-            <label for="your name">Your Name</label>
-            <input type="text" />
+          <label htmlFor="name">Your Name</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formValues.name}
+            onChange={handleInputChange}
+          />
         </div>
         <div>
-            <label for="your name">Email</label>
-            <input type="text" />
+          <label htmlFor="email">Email</label>
+          <input
+            type="text"
+            id="email"
+            name="email"
+            value={formValues.email}
+            onChange={handleInputChange}
+          />
         </div>
         <div>
-            <label for="your name">Title</label>
-            <input type="text" />
+          <label htmlFor="title">Title</label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            value={formValues.title}
+            onChange={handleInputChange}
+          />
         </div>
         <div>
-            <label for="your name">Body</label>
-            <input type="text" />
+          <label htmlFor="body">Body</label>
+          <textarea
+            id="body"
+            name="body"
+            value={formValues.body}
+            onChange={handleInputChange}
+          />
         </div>
-        <button class="button-pushable" id="next">
-                    <span class="button-shadow"></span>
-                    <span class="button-edge"></span>
-                    <span class="button-front text">
-                    Send via Email!
-                    </span>
-                </button>
+        <button type="submit" className="button-pushable" id="next">
+          <span className="button-shadow"></span>
+          <span className="button-edge"></span>
+          <span className="button-front text">Send via Email!</span>
+        </button>
       </form>
     </body>
   );
