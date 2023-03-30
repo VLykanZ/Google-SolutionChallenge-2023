fc55164b7b3d8547713ad30f2e9a0048afdc0dc4
VLykanZ <Suphawish707@gmail.com>
2023-03-31 04:17:24 +0700

    update store

--- src/store.jsx
+++ src/store.jsx
@@ -2,12 +2,15 @@ import { configureStore } from '@reduxjs/toolkit';
 
 const initialState = {
   bodyPart: '',
+  diagnoseScore: ''
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case 'SET_BODY_PART':
       return { ...state, bodyPart: action.payload };
+    case 'SET_DIAGNOSE_SCORE':
+    return { ...state, diagnoseScore: action.payload };
     default:
       return state;
   }
@@ -19,6 +22,10 @@ const store = configureStore({
 
 export const setBodyPart = (bodyPart) => {
   return { type: 'SET_BODY_PART', payload: bodyPart };
+};
+
+export const setDiagnoseScore = (diagnoseScore) => {
+  return { type: 'SET_DIAGNOSE_SCORE', payload: diagnoseScore };
 };
 
 export default store;
